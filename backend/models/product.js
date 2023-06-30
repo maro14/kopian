bcee67c80f8bb789fbef57bd6394de1738e890c1
brok22 <red0x1@localhost.localdomain>
2023-06-30 13:06:14 +0700

    product model

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,1 +1,11 @@
-git 
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: Sring
+})
+
+const User = mongoose.model('user', productSchema)
+
+module.exports = User
